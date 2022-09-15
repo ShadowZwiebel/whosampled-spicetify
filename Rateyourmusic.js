453df3115e3bb5437bad8fbe3402a56ff04ef19f
ShadowZwiebel <85753601+ShadowZwiebel@users.noreply.github.com>
2022-09-15 19:22:31 +0200

    Add files via upload

---
+++ Rateyourmusic.js
@@ -0,0 +1,24 @@
+(async function extension() {
+    const { Platform } = Spicetify;
+    if (!Platform) {
+      setTimeout(extension, 300);
+      return;
+    }
+
+    function openSite(){
+
+        var trackS = new String(Spicetify.Player.data.track.metadata.title)
+        
+
+        let searchTerm = trackS;
+        let searchLink = `https://www.whosampled.com/search/?q=${searchTerm}`;
+        let fckCustomEscapedLink = encodeURI(searchLink.replaceAll(' ', '+'));
+        window.open(fckCustomEscapedLink);
+
+
+      }
+
+    const cntxMenu = new Spicetify.ContextMenu.Item("rateyourmusic", openSite)
+    cntxMenu.register()
+
+})();
